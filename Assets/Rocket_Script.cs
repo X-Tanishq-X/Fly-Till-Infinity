7d0e57109a098efb43d9eb06b7b8f3f9510b4e3a
Siddharth-rajput2201 <rajputsiddharth18@gmail.com>
2020-10-01 00:13:08 +0530

    Added Left Side Movement
    Now Rocket Can Move Towards Left Hand Side

---
+++ Assets/Rocket_Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket_Script : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputprocess();
+    }
+    private void inputprocess()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        
+    }
+}
